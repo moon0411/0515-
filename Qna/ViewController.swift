14809200192d9740cc3617cb45ce1a294ffd83dc
Shr Ying Pan <apppeterpan@gmail.com>
2018-05-15 18:45:00 +0800

    Initial Commit

---
+++ Qna/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Qna
+//
+//  Created by SHIH-YING PAN on 2018/5/15.
+//  Copyright © 2018 SHIH-YING PAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
